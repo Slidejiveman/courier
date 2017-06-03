12bb79483810dc82567ae588119bd69c8be6973a
Slidejiveman <rdnotlaw91@outlook.com>
2017-06-03 15:21:50 -0500

    Adding database access object classes so that we will be able to take advantage of the database functionality quickly after Monday's lecture.

---
+++ src/courierdm/EmployeeDBAO.java
@@ -0,0 +1,12 @@
+package courierdm;
+
+/**
+ * This class will hold the database access code so that we can read values in
+ * from the database and populate the application lists with good data.
+ * 
+ * @author rdnot
+ *
+ */
+public class EmployeeDBAO {
+
+}
