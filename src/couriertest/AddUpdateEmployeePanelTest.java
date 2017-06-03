0a3261e47e60a841bccc3654a41b16f84fa5cbb8
Slidejiveman <rdnotlaw91@outlook.com>
2017-06-03 17:35:50 -0500

    Add a package to hold JUnit automated tests. Also, updated classpath.

---
+++ src/couriertest/AddUpdateEmployeePanelTest.java
@@ -0,0 +1,20 @@
+package couriertest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * A test method that will exercise the basic function of the
+ * Add and Update Panel for Employees.
+ * @author rdnot
+ *
+ */
+public class AddUpdateEmployeePanelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
