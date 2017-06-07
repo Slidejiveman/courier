26ca50f56f2c102e4388b1cb461a0aafefb2ad70
Slidejiveman <rdnotlaw91@outlook.com>
2017-06-07 16:47:37 -0500

    This init script will populate all of your tables with initial data!

--- lib/My_SQL_Initializer.sql
+++ lib/My_SQL_Initializer.sql
@@ -71,4 +71,51 @@ INSERT INTO `intersection`(`intersection_id`, `closed_end_date`,
   `closed_start_date`, `intersection_status`, `intersection_name`,
   `x_coordinate`, `y_coordinate`, `client_at_intersection`)
 VALUES (39,null,null,1,'D St. & 6th',3,5,1010);
+
+INSERT INTO `employee`(`employee_id`, `employee_role`, `employee_email`,
+  `employee_is_active`, `employee_name`, `employee_password`, `employee_shift`,
+  `employee_username`)
+VALUES (0,'Owner','acmecourierservicesowner@gmail.com',1,'David North','owner',
+  1,'owner');
+INSERT INTO `employee`(`employee_id`, `employee_role`, `employee_email`,
+  `employee_is_active`, `employee_name`, `employee_password`, `employee_shift`,
+  `employee_username`)
+VALUES (1,'Courier','jacksprat@gmail.com',1,'Jack Sprat','courier1',
+  1,'courier1');
+INSERT INTO `employee`(`employee_id`, `employee_role`, `employee_email`,
+  `employee_is_active`, `employee_name`, `employee_password`, `employee_shift`,
+  `employee_username`)
+VALUES (2,'Courier','peterpumpkin@gmail.com',1,'Peter Pumpkin','courier2',
+  3,'courier2');
+INSERT INTO `employee`(`employee_id`, `employee_role`, `employee_email`,
+  `employee_is_active`, `employee_name`, `employee_password`, `employee_shift`,
+  `employee_username`)
+VALUES (3,'Courier','jackcandle@gmail.com',1,'Jack Candle','courier3',
+  2,'courier3');
+INSERT INTO `employee`(`employee_id`, `employee_role`, `employee_email`,
+  `employee_is_active`, `employee_name`, `employee_password`, `employee_shift`,
+  `employee_username`)
+VALUES (4,'Courier','marymuffet@gmail.com',0,'Mary Muffet','courier4',
+  1,'courier4');
+INSERT INTO `employee`(`employee_id`, `employee_role`, `employee_email`,
+  `employee_is_active`, `employee_name`, `employee_password`, `employee_shift`,
+  `employee_username`)
+VALUES (5,'Courier','kencole@gmail.com',1,'Ken Cole','courier5',
+  2,'courier5');
+INSERT INTO `employee`(`employee_id`, `employee_role`, `employee_email`,
+  `employee_is_active`, `employee_name`, `employee_password`, `employee_shift`,
+  `employee_username`)
+VALUES (6,'OrderTaker','karenpowers@gmail.com',1,'Karen Powers','ordertaker1',
+  1,'ordertaker1');
+
+INSERT INTO `delivery_ticket`(`delivery_ticket_id`, `delivery_time`,
+  `departure_time`, `pick_up_time`, `return_time`, `estimated_blocks`,
+  `estimated_delivery_time`, `estimated_price`, `estimated_departure_time`,
+  `is_bill_pick_up`, `bonus_earned`, `order_date`, `requested_pickup_time`,
+  `special_instructions`, `ticket_status`, `dt_courier_id`, `dt_delivery_client_id`,
+  `dt_order_taker_id`, `dt_pick_up_client_id`)
+VALUES (0,'2017-06-07 10:45:00','2017-06-07 10:15:00','2017-06-07 10:30:00',
+  '2017-06-07 11:00:00',6,'2017-06-07 10:50:00',22.00,'2017-06-07 10:15:00',
+  1,1,'2017-06-07','2017-06-07 10:30:00','Go to the 2nd floor.',
+  'Closed',3,1004,6,1005)
 SET FOREIGN_KEY_CHECKS=1;
