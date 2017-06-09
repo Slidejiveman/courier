6c0593e820fbc527c1057e38a6df0a71ae341e3c
DESKTOP-KSDU36P\Augustin Nshimiyiman <addeodatus01@gmail.com>
2017-06-09 15:49:11 -0500

    New Commit

--- src/courierdm/IntersectionDBAO.java
+++ src/courierdm/IntersectionDBAO.java
@@ -31,12 +31,6 @@ public class IntersectionDBAO {
 	 */
 	public static List<Intersection> listIntersections() {
 		TypedQuery<Intersection> query = CourierEntityManager.getEntityManager().createQuery("SELECT intersection FROM intersection intersection", Intersection.class);
-		List<Intersection> queryResult= query.getResultList();
-		for(Intersection intersection: queryResult){
-			if(!intersection.getIsOpen()){
-				intersection.setClosedDateStart(DateParser.parseDatabaseDate(intersection.getClosedDateStart()));
-			}
-		}
 		return query.getResultList();
 	}
 	
